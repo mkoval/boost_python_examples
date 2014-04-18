5bb93be595a12c611dc99d255c9bebbe975819f0
Mike Koval <mkoval@cs.cmu.edu>
2014-04-18 02:11:24 -0400

    Added to_python_converter example.

--- src/bp_class.cpp
+++ src/bp_class.cpp
@@ -12,6 +12,28 @@ static std::string hello_world()
     return "Hello World";
 }
 
+struct Point {
+    double x, y;
+};
+
+Point make_point(double x, double y) {
+    Point p;
+    p.x = x;
+    p.y = y;
+    return p;
+}
+
+struct Point_to_python {
+    static PyObject *convert(Point const &pt)
+    {
+        using namespace boost::python;
+        object py_pt = make_tuple(pt.x, pt.y);
+        return incref(object(py_pt).ptr());
+    }
+};
+
+
+
 BOOST_PYTHON_MODULE(bp_class)
 {
     using namespace boost::python;
@@ -21,4 +43,8 @@ BOOST_PYTHON_MODULE(bp_class)
         .def("hello", &HelloWorld::hello)
         .def("world", &HelloWorld::world)
         ;
+
+    def("make_point", &make_point);
+
+    to_python_converter<Point, Point_to_python>();
 }
