788bbe4b8c3ec4942fedd9ec09cc8f3272806747
Mike Koval <mkoval@cs.cmu.edu>
2014-04-18 01:55:14 -0400

    Added a Boost.Python class example.

--- pythonsrc/bp_class.py
+++ pythonsrc/bp_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 import bp_class
 print 'Function =', bp_class.hello_world()
+
+x = bp_class.HelloWorld()
+print 'Class =', x.hello(), x.world()
--- src/bp_class.cpp
+++ src/bp_class.cpp
@@ -1,6 +1,12 @@
 #include <string>
 #include <boost/python.hpp>
 
+class HelloWorld {
+public:
+    std::string hello() const { return "Hello"; }
+    std::string world() const { return "World"; }
+};
+
 static std::string hello_world()
 {
     return "Hello World";
@@ -10,4 +16,9 @@ BOOST_PYTHON_MODULE(bp_class)
 {
     using namespace boost::python;
     def("hello_world", &hello_world);
+
+    class_<HelloWorld>("HelloWorld")
+        .def("hello", &HelloWorld::hello)
+        .def("world", &HelloWorld::world)
+        ;
 }
